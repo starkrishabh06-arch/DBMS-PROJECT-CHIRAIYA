018135e651bcd46cb0d492c749c40e54639f1d68
starkrishabh06-arch <starkrishabh06@gmail.com>
2026-04-13 21:48:52 +0530

    Update expenditure.sql

--- expenditure.sql
+++ expenditure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2023 at 11:46 AM
+-- Generation Time: May 02, 2023 at 11:46 AM 
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
